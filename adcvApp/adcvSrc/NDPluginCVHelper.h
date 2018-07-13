3d01e22f26993a9171b2cc788d7417770c9a260c
Jakub Wlodek <jwlodek@cs.stonybrook.edu>
2018-07-13 14:26:00 -0400

    updated helper header file to create class

--- adcvApp/adcvSrc/NDPluginCVHelper.h
+++ adcvApp/adcvSrc/NDPluginCVHelper.h
@@ -20,16 +20,29 @@
 //OpenCV is used for image manipulation
 #include <opencv2/opencv.hpp>
 
-//function that will print information about an opencv error
-void print_cv_error(Exception &e);
 
-//function that will perform Canny edge detection
-Mat edge_detector_canny(Mat &img, int threshVal, int threshRatio, int blurDegree);
+class NDPluginCVHelper {
 
-//function that will perform Laplacian edge detection
-Mat edge_detector_laplacian(Mat &img, int blurDegree);
+    public:
 
-//function that will find contours and the centroids of them in a given ROI
-Mat centroid_finder(Mat &img, int roiX, int roiY, int roiWidth, int roiHeight, int blurDegree, int threshVal);
+        NDPluginCVHelper();
+        ~NDPluginCVHelper();
 
+        //function that will print information about an opencv error
+        void print_cv_error(Exception &e);
+
+        //function that will perform Canny edge detection
+        Mat edge_detector_canny(Mat &img, int threshVal, int threshRatio, int blurDegree);
+
+        //function that will perform Laplacian edge detection
+        Mat edge_detector_laplacian(Mat &img, int blurDegree);
+
+        //function that will find contours and the centroids of them in a given ROI
+        Mat centroid_finder(Mat &img, int roiX, int roiY, int roiWidth, int roiHeight, int blurDegree, int threshVal);
+    
+    protected:
+
+    private:
+
+};
 #endif
