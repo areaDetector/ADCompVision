d02f73a943de141412231d2fed4c82d291bd700f
jwlodek <jwlodek@cs.stonybrook.edu>
2018-11-21 16:31:15 -0500

    added some more explanation on how to add to the plugin, cleaned up comments

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -5,3 +5,56 @@ Some pointers:
 * OpenCV uses Mat objects for images. They are written as a 'smart-pointer' class, meaning that they are passed
 by-reference by default. Because of this, make sure to pass the image into the function with (Mat &img).
 * The plugin works by calling functions in a helper library. Eventually, a 'custom-function' option will be added so that a custom use case can be easily added and supported.
+
+# Adding a new CV function
+
+To add a new CV function there are several files you will need to edit. First, in the NDCV.template file, find the records that store the CV functions. You will need to add your function to one of these PVs. Follow the rules for EPICS multi-bit-binary-outputs/inputs to add your function to the PV.   
+Next, you will need to edit the NDPluginCVHelper.cpp and NDPluginCVHelper.h files. In NDPluginCVHelper.h, find the definition of ADCVFunction_t and add:
+```
+// Some basic flag types
+typedef enum {
+    ADCV_NoFunction         = 0,
+    ADCV_EdgeDetectionCanny = 1,
+    ADCV_Threshold          = 2,
+    ADCV_YOURFUNCTION       = n,
+} ADCVFunction_t;
+```
+where n is the next integer. This will add the function to the list of possible functions handled by ADCompVision. Then, add the line:
+
+```
+ADCVStatus_t YOURFUNCTION(Mat &img, double* inputs, double* outputs);
+```
+in the 'public' portion of the class declaration. You may follow the standard set by the other functions.   
+Next, in the NDPluginCVHelper.cpp file, add your new function definition. it should take the following form:
+```
+ADCVStatus_t NDPluginCVHelper::YOURFUNCTION(Mat &img, double* inputs, double* outputs){
+    const char* functionName = "YOURFUNCTION";
+    ADCVStatus_t status = cvHelperSuccess;
+    param1 = inputs[0];
+    param2 = inputs[1];
+    .
+    .
+    .
+
+    try{
+        // Process your image here
+        // Don't make copies, pass img, img as input and output to OpenCV.
+        // Set output values with output[n] = value. cast non-double values to double
+        // If you need more inputs or outputs, add more PVs following previous examples.
+    }catch(Exception &e){
+        print_cv_error(e, functionName);
+        status = cvHelperError;
+    }
+
+    return status;
+}
+```
+Finally, you need to edit the 'processImage' function in NDPluginCVHelper.cpp. In the switch statement, add a case as follows:
+
+```
+case ADCV_YOURFUNCTION:
+    status = YOURFUNCTION(image, inputs, outputs);
+    break;
+```
+
+Your function should now be implemented into ADCompVision and is ready to be tested.
--- adcvApp/adcvSrc/NDPluginCVHelper.cpp
+++ adcvApp/adcvSrc/NDPluginCVHelper.cpp
@@ -53,14 +53,40 @@ void NDPluginCVHelper::print_cv_error(Exception &e, const char* functionName){
 */
 
 
+//------------- Template for OpenCV function wrapper -------------------
+
+/*
+ADCVStatus_t NDPluginCVHelper::YOURFUNCTION(Mat &img, double* inputs, double* outputs){
+    const char* functionName = "YOURFUNCTION";
+    ADCVStatus_t status = cvHelperSuccess;
+    param1 = inputs[0];
+    param2 = inputs[1];
+    .
+    .
+    .
+
+    try{
+        // Process your image here. Do not make copies, simply pass img as the input and output of theOpenCV calls
+    }catch(Exception &e){
+        print_cv_error(e, functionName);
+        status = cvHelperError;
+    }
+    return status;
+}
+*/
+
+//------------- OpenCV function wrapper implementations -------------------
+
+
 /**
+ * WRAPPER      -> canny_edge_detection
  * Function for canny-based edge detection
  * 
- * @inCount      -> 3
- * @inFormat     -> [Threshold value (Int), Threshold ratio (Int), Blur degree (Int)]
- * 
- * @outCount     -> TODO
- * @outFormat    -> TODO
+ * @inCount     -> 3
+ * @inFormat    -> [Threshold value (Int), Threshold ratio (Int), Blur degree (Int)]
+ *
+ * @outCount    -> TODO
+ * @outFormat   -> TODO
  */
 ADCVStatus_t NDPluginCVHelper::canny_edge_detection(Mat &img, double* inputs, double* outputs){
     const char* functionName = "canny_edge_detection";
@@ -83,14 +109,16 @@ ADCVStatus_t NDPluginCVHelper::canny_edge_detection(Mat &img, double* inputs, do
     return status;
 }
 
+
 /**
- * Function for canny-based edge detection
+ * WRAPPER      -> laplacian_edge_detection
+ * Function for laplacian-based edge detection
  * 
- * @inCount      -> 1
- * @inFormat     -> [Blur degree (Int)]
+ * @inCount     -> 1
+ * @inFormat    -> [Blur degree (Int)]
  * 
- * @outCount     -> TODO
- * @outFormat    -> TODO
+ * @outCount    -> TODO
+ * @outFormat   -> TODO
  */
 ADCVStatus_t NDPluginCVHelper::laplacian_edge_detection(Mat &img, double* inputs, double* outputs){
     const char* functionName = "laplacian_edge_detection";
@@ -115,6 +143,7 @@ ADCVStatus_t NDPluginCVHelper::laplacian_edge_detection(Mat &img, double* inputs
 
 
 /**
+ * WRAPPER      -> threshold_image
  * Function that thresholds an image based on a certain pixel value
  * 
  * @inCount     -> 3
@@ -126,9 +155,9 @@ ADCVStatus_t NDPluginCVHelper::laplacian_edge_detection(Mat &img, double* inputs
 ADCVStatus_t NDPluginCVHelper::threshold_image(Mat &img, double* inputs, double* outputs){
     const char* functionName = "threshold_image";
     ADCVStatus_t status = cvHelperSuccess;
-    int threshVal = inputs[0];
-    int threshMax = inputs[1];
-    int threshType = inputs[2];
+    int threshVal = (int) inputs[0];
+    int threshMax = (int) inputs[1];
+    int threshType = (int) inputs[2];
     try{
         threshold(img, img, threshVal, threshMax, threshType);
     }catch(Exception &e){
@@ -139,11 +168,25 @@ ADCVStatus_t NDPluginCVHelper::threshold_image(Mat &img, double* inputs, double*
 }
 
 
+/**
+ * WRAPPER      -> find_centroids
+ * Function for finding centroids of objects in an image. Useful for alignment of objects
+ * First, blur the object based on a certain blur degree (kernel size). Then threshold the image
+ * based on a certain threshold value. Then find contours in the image using the findContours()
+ * function. Then get the centroids from the contour objects. Draw the contours and centroids on 
+ * the image. Set the first 5 centroid coordinates to the output values.
+ * 
+ * @inCount     -> 2
+ * @inFormat    -> [Blur Degree (Int), Threshold Value (Int)]
+ * 
+ * @outCount    -> ?
+ * @outFormat   -> [CentroidX (Double), CentroidY (Double) ... ]
+ */
 ADCVStatus_t NDPluginCVHelper::find_centroids(Mat &img, double* inputs, double* outputs){
     static const char* functionName = "find_centroids";
     ADCVStatus_t status = cvHelperSuccess;
-    int blurDegree = inputs[0];
-    int thresholdVal = inputs[1];
+    int blurDegree = (int) inputs[0];
+    int thresholdVal = (int) inputs[1];
     try{
         GaussianBlur(img, img, Size(blurDegree, blurDegree), 0);
         threshold(img, img, thresholdVal, 255, THRESH_BINARY);
@@ -182,54 +225,8 @@ ADCVStatus_t NDPluginCVHelper::find_centroids(Mat &img, double* inputs, double* 
     return status;
 }
 
+
 //------------------------ End of OpenCV wrapper functions -------------------------------------------------
-
-
-/*
-
-    TODO: Rewrite this function/test chainging core functions to achieve the same result
-
- * Function that finds centroid of all contours in a given region of interest.
- * The following process is taken:
- * Gaussian blur -> threshold -> crop -> find contours+moments -> find centroids
- * 
- * @params: img -> image to be processed
- * @params: roiX -> x-coordinate of upper left corner of ROI (region of interest)
- * @params: roiY -> y-coordinate of upper left corner of ROI
- * @params: roiWidth -> width of the ROI
- * @params: roiHeight -> height of the ROI. Note that the height in an image is measured from the top down
- * @params: blurDegree -> size of kernel in Gaussian blur
- * @params: threshVal -> cutoff  for threshold
- * @return: cropped image with detected contours and centroids
-Mat NDPluginCVHelper::centroid_finder(Mat &img, int roiX, int roiY, int roiWidth, int roiHeight, int blurDegree, int threshVal){
-    Mat afterBlur, afterThresh, afterCrop, cropOriginal;
-    GaussianBlur(img, afterBlur, Size(blurDegree,blurDegree), 0);
-    threshold(afterBlur, afterThresh, threshVal, 255, THRESH_BINARY);
-    Rect myROI(roiX, roiY, roiWidth, roiHeight);
-    afterCrop = afterThresh(myROI);
-    cropOriginal = img(myROI);
-    vector<vector<Point>> contours;
-    vector<Vec4i> hierarchy;
-
-    findContours(afterCrop, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0,0));
-    vector<Moments> contour_moments(contours.size());
-    for(int i = 0; i< contours.size(); i++){
-        contour_moments[i] = moments(contours[i], false);
-    }
-    vector<Point2f> contour_centroids(contours.size());
-    for(int i = 0; i< contours.size(); i++){
-        contour_centroids[i] = Point2f((contour_moments[i].m10/contour_moments[i].m00), (contour_moments[i].m01/contour_moments[i].m00));
-    }
-    for(int i = 0; i< contours.size(); i++){
-        drawContours(cropOriginal, contours, i, Scalar(0,255,0), 2, 8, hierarchy, 0, Point());
-        circle(cropOriginal, contour_centroids[i], 3, Scalar(255,0,0), -1, 8, 0);
-    }
-    imshow("contours+centroids", cropOriginal);
-    waitKey(0);
-    return img;
-}
-
-*/
 
 
 /**
